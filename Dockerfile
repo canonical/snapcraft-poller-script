7e355dbaf3046f20fd4ed2840612e0a1d187c751
Francisco Jiménez Cabrera <francisco.jimenez.cabrera@canonical.com>
2020-04-01 15:02:53 +0100

    Added Sentry (#9)

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM ubuntu:focal
 
 # Install required system packages for our CronJob
-RUN apt-get update && apt-get install --yes --no-install-recommends python3-pip
+RUN apt-get update && apt-get install --yes --no-install-recommends python3-pip python3-setuptools python3-dev python3-wheel
 
 ADD . .
 RUN pip3 install --no-cache-dir -r requirements.txt
--- poller.py
+++ poller.py
@@ -4,15 +4,21 @@ import argparse
 import datetime
 import logging.config
 import os
-import time
 
 import dateutil.parser
+import logaugment
 from canonicalwebteam.launchpad import Launchpad
 from requests import Session
+from sentry_sdk import capture_exception, init
+from sentry_sdk.integrations.logging import ignore_logger
 
 from src import helper
 from src.exceptions import GitHubRateLimit, InvalidGitHubRepo
 from src.github import GitHub
+
+# Set up Sentry
+init(os.getenv("SENTRY_DSN"))
+ignore_logger("script.output")
 
 launchpad = Launchpad(
     username="build.snapcraft.io",
@@ -21,7 +27,7 @@ launchpad = Launchpad(
     session=Session(),
 )
 
-github = GitHub(os.getenv("GITHUB_SNAPCRAFT_USER_TOKEN"), Session())
+github = GitHub(os.getenv("GITHUB_SNAPCRAFT_POLLER_TOKENS").split(), Session())
 
 # Skip Snaps built in the last 24 hours
 threshold = datetime.datetime.now() - dateutil.relativedelta.relativedelta(
@@ -29,25 +35,25 @@ threshold = datetime.datetime.now() - dateutil.relativedelta.relativedelta(
 )
 
 
-def needs_building(snap, logging):
+def needs_building(snap, logger):
     if not snap["store_name"]:
-        logging.info(f"Launchpad snap {snap['name']} doesn't have store name")
+        logger.info(f"Launchpad snap {snap['name']} doesn't have store name")
         return False
 
     snap_name = snap["store_name"]
 
     if not snap["store_upload"]:
-        logging.info(f"{snap_name}: Can't be publish from Launchpad")
+        logger.info(f"{snap_name}: Can't be publish from Launchpad")
         return False
 
     if not github.is_github_repository_url(snap["git_repository_url"]):
-        logging.info(f"{snap_name}: Is not ussing GitHub")
+        logger.info(f"{snap_name}: Is not ussing GitHub")
         return False
 
     gh_link = snap["git_repository_url"][19:]
     gh_owner, gh_repo = gh_link.split("/")
 
-    logging.debug(f"Verifying snap {snap_name} with GitHub repo {gh_link}")
+    logger.debug(f"Verifying snap {snap_name} with GitHub repo {gh_link}")
 
     try:
         yaml_file = github.get_snapcraft_yaml_location(gh_owner, gh_repo)
@@ -55,39 +61,35 @@ def needs_building(snap, logging):
             gh_owner, gh_repo, yaml_file, snap_name
         )
     except InvalidGitHubRepo as e:
-        logging.info(f"Snap {snap_name} SKIPPED: {str(e)}")
+        logger.info(f"Snap {snap_name} SKIPPED: {str(e)}")
         return False
 
-    last_build = helper.get_last_build_date(launchpad, snap_name, logging)
+    last_build = helper.get_last_build_date(launchpad, snap_name, logger)
 
     if not last_build:
-        logging.info(
-            f"Snap {snap_name} SKIPPED: The snap has never been built"
-        )
+        logger.info(f"Snap {snap_name} SKIPPED: The snap has never been built")
         return False
 
     if last_build > threshold.timestamp():
-        logging.info(
+        logger.info(
             f"Snap {snap_name} SKIPPED: The snap has been recently built"
         )
         return False
 
-    logging.debug(f"Checking if the repo has been updated since last build")
+    logger.debug(f"Checking if the repo has been updated since last build")
     try:
         if github.has_repo_changed_since(gh_owner, gh_repo, last_build):
-            logging.debug(
-                f"Snap {snap_name} repo has changed since last build"
-            )
+            logger.debug(f"Snap {snap_name} repo has changed since last build")
             return True
     except InvalidGitHubRepo as e:
-        logging.debug(f"Snap {snap_name} SKIPPED: {str(e)}")
+        logger.debug(f"Snap {snap_name} SKIPPED: {str(e)}")
         return False
 
-    logging.debug(f"Getting defined parts for snap {snap_name}")
+    logger.debug(f"Getting defined parts for snap {snap_name}")
     parts = github.get_defined_parts(gh_owner, gh_repo, yaml_file)
 
-    if helper.has_parts_changed(github, snap_name, parts, last_build, logging):
-        logging.debug(f"Snap {snap_name} parts needs building")
+    if helper.has_parts_changed(github, snap_name, parts, last_build, logger):
+        logger.debug(f"Snap {snap_name} parts needs building")
         return True
 
     return False
@@ -115,37 +117,38 @@ if __name__ == "__main__":
     if args.verbose:
         logging_level = logging.DEBUG
 
-    # Disable logs from imported modules
-    logging.config.dictConfig({"version": 1, "disable_existing_loggers": True})
-    logging.basicConfig(
-        format="%(asctime)s - %(levelname)s - %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
-        level=logging_level,
-    )
+    logger = helper.get_logger(logging_level)
+    snaps = helper.get_all_snaps(launchpad, logger)
+    current_snap = 0
 
-    for snap in helper.get_all_snaps(launchpad, logging):
+    for snap in snaps:
+        current_snap += 1
+        logaugment.add(logger, current_snap=current_snap)
+
         try:
-            if needs_building(snap, logging):
-                logging.debug(f"Snap {snap['store_name']} needs building")
+            if needs_building(snap, logger):
+                logger.debug(f"Snap {snap['store_name']} needs building")
 
                 if launchpad.is_snap_building(snap["store_name"]):
-                    logging.debug(
+                    logger.debug(
                         f"Snap {snap['store_name']} is already being build"
                     )
                 else:
-                    logging.warning(f"Snap {snap['store_name']} is building")
+                    logger.warning(f"Snap {snap['store_name']} is building")
                     launchpad.build_snap(snap["store_name"])
-        except GitHubRateLimit:
-            wait = github.date_reset_limit - int(
-                datetime.datetime.now().timestamp()
-            )
-            logging.warning(
-                f"GitHub API rate limit exceeded - waiting {wait} seconds"
-            )
-            if wait > 0:
-                time.sleep(wait)
-
+        except GitHubRateLimit as e:
+            logger.error("GitHub API rate limit exceeded")
+            # Raise the exception to abort the script and catch it on Sentry
+            raise e
         except Exception as e:
-            logging.error(
+            # Extra info for Sentry
+            e.snap_launchpad_name = snap["name"]
+            e.snap_name = snap["store_name"]
+            e.snap_github_repo = snap["git_repository_url"]
+
+            # Send this exception to Sentry but script will continue
+            capture_exception(e)
+
+            logger.error(
                 f"An error occurrent with snap {snap['store_name']}: {str(e)}"
             )
